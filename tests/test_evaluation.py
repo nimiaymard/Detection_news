25e7e1a898a854de55e053d5a7555eaf10b1a355
NIMI <nimichristbert@gmail.com>
2024-08-11 23:08:56 +0200

    Modification encore test_evaluation.py

--- tests/test_evaluation.py
+++ tests/test_evaluation.py
@@ -2,7 +2,7 @@ import unittest
 import sys
 import os
 
-# Ajouter le répertoire racine du projet au PYTHONPATH
+# Ajout du répertoire racine du projet au PYTHONPATH
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 
 from src.models.evaluate_model import evaluate_model
