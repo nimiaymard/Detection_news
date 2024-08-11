19cf2932665ceca5a076519134207d5389a63d7a
NIMI <nimichristbert@gmail.com>
2024-08-11 23:06:18 +0200

    Modification test_evaluation.py

--- tests/test_evaluation.py
+++ tests/test_evaluation.py
@@ -1,33 +1,24 @@
-
 import unittest
+import sys
 import os
-import pandas as pd
+
+# Ajouter le répertoire racine du projet au PYTHONPATH
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+
 from src.models.evaluate_model import evaluate_model
 
 class TestEvaluateModel(unittest.TestCase):
     
     def setUp(self):
-        # Chemin vers le fichier de modèle de test
+        # Chemins vers les fichiers de test
         self.model_filepath = os.path.abspath(os.path.join(os.path.dirname(__file__), '../saved_models/svm_model.pkl'))
-        # Chemin vers le fichier de test de données
-        self.test_filepath = os.path.abspath(os.path.join(os.path.dirname(__file__), '../data/processed/sample_data_transformed_test.csv'))
-
-        # Vérifier que les fichiers existent
-        self.assertTrue(os.path.exists(self.model_filepath), f"Model file not found: {self.model_filepath}")
-        self.assertTrue(os.path.exists(self.test_filepath), f"Test data file not found: {self.test_filepath}")
+        self.test_filepath = os.path.abspath(os.path.join(os.path.dirname(__file__), '../data/processed/mabs_transformed.csv'))
 
     def test_evaluate_model(self):
-        # Appeler la fonction d'évaluation
         accuracy, report = evaluate_model(self.model_filepath, self.test_filepath)
         
-        # Vérifier que les résultats d'évaluation sont retournés
         self.assertIsInstance(accuracy, float)
-        self.assertGreater(accuracy, 0, "Accuracy should be greater than 0")
-        self.assertLessEqual(accuracy, 1, "Accuracy should be less or equal to 1")
-
         self.assertIsInstance(report, str)
-        self.assertIn("Fake_news", report)
-        self.assertIn("Good_news", report)
         
         print(f"Accuracy: {accuracy}")
         print("Classification Report:\n", report)
