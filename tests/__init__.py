c2cf2595a169c7140ee69a37873060163d3a93a1
NIMI <nimichristbert@gmail.com>
2024-08-11 22:53:38 +0200

    Modification de __init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,10 +1,4 @@
-# src/features/__init__.py
 # tests/__init__.py
-
-import sys
-import os
-
-# Ajouter le répertoire racine du projet au PYTHONPATH
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+# Importer ici les fonctions ou classes communes à plusieurs fichiers de tests, si nécessaire.
 
 
